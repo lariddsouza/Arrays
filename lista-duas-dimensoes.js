495d54578a48661d2d16c791a65c3860fd195cf4
lariddsouza <larissaddsouza@gmail.com>
2023-03-27 19:07:02 -0300

    novos testes com arrays

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,6 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`O aluno da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]} e a nota dessa aluna é ${listaDeAlunosEMedias[1][1]}`);
